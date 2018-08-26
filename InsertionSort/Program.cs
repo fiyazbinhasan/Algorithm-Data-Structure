c618847fdd3698281ae668fd9a1ab0704fe4f816
fiyazbinhasan <fiyazhasan@live.com>
2018-08-26 18:22:09 +0600

    Insertion sort added

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Utility;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Start with everything after the first element is unsorted */
+            /* Array with a single element is always sorted */
+            /* Check the current element value with the previous one */
+            /* Clauses: Swap if the previous element is greater than the current element 
+             * and the current element index is greater than 0 */
+
+            var arr = IOUtility.CreateArryWithElements();
+
+            for(int i = 1; i < arr.Length; i++)
+            {
+                var value = arr[i];
+
+                var step = i;     
+
+                while (step > 0 && arr[step - 1] > value)
+                {
+                    var temp = arr[step - 1];
+                    arr[step - 1] = arr[step];
+                    arr[step] = temp;
+
+                    step--;
+                }
+
+                arr[step] = value;
+            }
+
+            IOUtility.PrintArrayElements(arr);
+        }
+    }
+}
---
+++ Utility/IOUtility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility
+{
+    static class IOUtility
+    {
+        public static int[] CreateArryWithElements()
+        {
+            Console.WriteLine("Number of elements?");
+            Int32 n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-----------------------");
+
+            int[] arr = new int[n];
+
+            int i = 0;
+            while(i < arr.Length)
+            {
+                Console.Write($"{i}th element:\t");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                i++;
+            }
+            return arr;
+        }
+
+        public static void PrintArrayElements(int[] arr)
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Sorted Array:");
+            int i = 0;
+            while (i < arr.Length)
+            {
+                Console.Write($"{arr[i]}\t");
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
